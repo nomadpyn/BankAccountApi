be1c069a3a5d091e0fd6383208887a6132bbed5b
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2023-10-28 16:09:22 +0700

    Add controllers

---
+++ BankAccountApi/Controllers/BankController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankAccountApi.Models;
+using BankAccountApi.Models.Dto;
+using BankAccountApi.Services;
+using BankAccountApi.Services.Interfaces;
+using BankAccountApi.Utils;
+
+namespace BankAccountApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankAccountController : Controller
+    {        
+        private readonly IBankWorker _transactionWorker;
+
+        public BankAccountController(IBankWorker transactionWorker)
+        {            
+            _transactionWorker = transactionWorker;
+        }
+
+        [HttpGet("{accountNumber}")]        
+        public async Task<IActionResult> GetCurrentAccount(int accountNumber)
+        {
+            if(!CheckValue.ValidateAccountNumber(accountNumber)) 
+            {
+                return BadRequest("Не верный формат номера банковского счета");
+            }
+
+            BankAccount? result = await _transactionWorker.GetBankAccountByAccountNumber(accountNumber);
+
+            if(CheckValue.CheckEmptyBankAccount(result))
+            {
+                return NoContent();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("balance/{accountNumber}")]
+        public async Task<IActionResult> GetAmountByAccount(int accountNumber)
+        {
+            if (!CheckValue.ValidateAccountNumber(accountNumber))
+            {
+                return BadRequest("Не верный формат номера банковского счета");
+            }
+
+            BankAccount? result = await _transactionWorker.GetBankAccountByAccountNumber(accountNumber);
+
+            if (CheckValue.CheckEmptyBankAccount(result))
+            {
+                return NoContent();
+            }
+
+            return Ok(result.Amount);
+        }
+
+        [HttpPost("createBankAccount")]
+        public async Task<IActionResult> CreateBankAccount([FromBody] BankAccount bankAccount)
+        {
+            if (!CheckValue.CheckNewBankAccount(bankAccount))
+            {
+                return BadRequest("Не корректные данные банковского счета");
+            }
+
+            BankAccount? result = await _transactionWorker.CreateBankAccount(bankAccount);
+
+            if (CheckValue.CheckEmptyBankAccount(result))
+            {
+                return BadRequest("Ошибка создания банковского счета");
+            }
+
+            return Ok(result);
+        }
+
+        
+
+    }
+}
---
+++ BankAccountApi/Controllers/TransactionController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankAccountApi.Models.Dto;
+using BankAccountApi.Models;
+using BankAccountApi.Services;
+using BankAccountApi.Utils;
+using BankAccountApi.Services.Interfaces;
+
+namespace BankAccountApi.Controllers
+{
+    
+        [ApiController]
+        [Route("api/[controller]")]
+        public class TransactionController : Controller
+        {
+        private readonly IBankWorker _transactionWorker;
+        public TransactionController(IBankWorker transactionWorker)
+        {
+            _transactionWorker = transactionWorker;
+        }
+        [HttpPost("createTransaction")]
+        public async Task<IActionResult> CreateTransaction([FromBody] TransactionModel transactionModel)
+        {
+            if (!CheckValue.ValidateTransactionModel(transactionModel))
+            {
+                return BadRequest("Неверный формат транзакции");
+            }
+
+            BankTransaction? result = await _transactionWorker.CreateBankTransaction(transactionModel);
+
+
+            if (CheckValue.CheckEmptyBankTransaction(result))
+            {
+                return BadRequest("Ошибка проведения транзакции");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("transactions")]
+        public async Task<IActionResult> GetAllTransactions()
+        {
+            IEnumerable<BankTransactionDto> result = await _transactionWorker.GetAllTransactions();
+
+            if (result.Count() <= 0)
+            {
+                return BadRequest("Нет транзакции для отображения");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("transactions/{accountNumber}")]
+        public async Task<IActionResult> GetTransactionsByAccountNumber(int accountNumber)
+        {
+            IEnumerable<BankTransactionDto> result = await _transactionWorker.GetTransactionsById(accountNumber);
+
+            if (result.Count() <= 0)
+            {
+                return BadRequest("Нет транзакции для отображения");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("transactionsbypage")]
+        public async Task<IActionResult> GetAllTransactionsByPage([FromQuery]PageParameters pageParameters)
+        {
+            IEnumerable<BankTransactionDto> result = await _transactionWorker.GetAllTransactionsByPage(pageParameters);
+
+            if (result.Count() <= 0)
+            {
+                return BadRequest("Нет транзакции для отображения");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("transactionsbypage/{accountNumber}")]
+        public async Task<IActionResult> GetTransactionsByAccountNumberByPage(int accountNumber, [FromQuery] PageParameters pageParameters)
+        {
+            IEnumerable<BankTransactionDto> result = await _transactionWorker.GetTransactionsByIdByPage(accountNumber, pageParameters);
+
+            if (result.Count() <= 0)
+            {
+                return BadRequest("Нет транзакции для отображения");
+            }
+
+            return Ok(result);
+        }
+
+    }
+}
