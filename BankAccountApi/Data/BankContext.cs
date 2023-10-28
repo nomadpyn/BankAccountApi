689989643627d47ff87c83cccfdb6fc0abf7e1bb
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2023-10-28 15:57:09 +0700

    Add DbContext Add Migrations

---
+++ BankAccountApi/Data/BankContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BankAccountApi.Models;
+
+namespace BankAccountApi.Data
+{
+    public class BankContext : DbContext
+    {
+        public DbSet<BankAccount> BankAccounts { get; set; }
+
+        public DbSet<BankTransaction> BankTransactions { get; set; }
+
+        public BankContext(DbContextOptions<BankContext> options) : base(options) { }
+    }
+}
---
+++ BankAccountApi/Migrations/20231028015702_Initial.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BankAccountApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AccountNumber = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BankTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TransactionTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    TransactionSign = table.Column<string>(type: "text", nullable: true),
+                    TransactionSum = table.Column<decimal>(type: "numeric", nullable: false),
+                    BalanceAfterTransaction = table.Column<decimal>(type: "numeric", nullable: false),
+                    BankAccountId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankTransactions_BankAccounts_BankAccountId",
+                        column: x => x.BankAccountId,
+                        principalTable: "BankAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankTransactions_BankAccountId",
+                table: "BankTransactions",
+                column: "BankAccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankTransactions");
+
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+        }
+    }
+}
